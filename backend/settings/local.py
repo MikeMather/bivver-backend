458dae0ab0bab61a6fc9a37ebde557f5d3c3d0f4
Mike Mather <17606330+MikeMather@users.noreply.github.com>
2020-04-08 20:21:12 -0230

    Update local.py

--- backend/settings/local.py
+++ backend/settings/local.py
@@ -7,7 +7,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'bivver_local',
         'USER': 'root',
-        'PASSWORD': 'citrus',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306'
     }
